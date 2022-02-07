ae7980825d3c7450ecea90e97d5956d2e42fe199
daisuke0925 <d.yakyu.0925@icloud.com>
2022-02-07 15:16:20 +0000

    ユーザーのマイページ、登録情報編集、プロフィール画像

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @photos = @user.photos
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :profile_image)
   end
 end
