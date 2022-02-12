399620c57d92670f06693c1995b6ebf972e6f5ea
daisuke0925 <d.yakyu.0925@icloud.com>
2022-02-12 12:34:58 +0000

    いいねの非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     @photo = Photo.find(params[:photo_id])
     favorite = current_user.favorites.new(photo_id: @photo.id)
     favorite.save
-    redirect_to photo_path(@photo)
+    # redirect_to photo_path(@photo)
   end
 
   def destroy
     @photo = Photo.find(params[:photo_id])
     favorite = current_user.favorites.find_by(photo_id: @photo.id)
     favorite.destroy
-    redirect_to photo_path(@photo)
+    # redirect_to photo_path(@photo)
   end
 end
